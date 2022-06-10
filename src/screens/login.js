f1ba8650f0b3360c8dd5a5c39e8a297a8b9d5b4d
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-06-10 11:41:47 -0300

    tailwind css

--- src/screens/login.js
+++ src/screens/login.js
@@ -3,5 +3,12 @@ import { Image, View, Text, Form , Input} from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 
 export default function Login () {
-return( <View></View>)
+return( 
+<View>
+    <image
+    source={require("assets/icon.png")}
+    />
+</View>
+);
+
 }
