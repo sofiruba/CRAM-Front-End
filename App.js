18b3c377ae9a1695dbe7e4c5ea29bfe67c01f5a7
Delfi <delfinamunich@gmail.com>
2022-06-23 00:57:17 -0300

    drawer

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native';
 import LoginScreen from './src/screens/login.js';
-import HomeScreen from './src/screens/home.js'
+import HomeScreen from './src/screens/home.js';
+import Main from './navigation/main.js';
 import { NavigationContainer } from '@react-navigation/native';
-
 import { TailwindConfig } from './tailwind.config';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Stack = createNativeStackNavigator();
-
+const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Main">
+      <Drawer.Screen name="Login" component={LoginScreen} />
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
-  );
+  )
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- navigation/main.js
+++ navigation/main.js
@@ -4,6 +4,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../src/screens/login';
 import HomeScreen from '../src/screens/home';
 const Stack = createNativeStackNavigator();
-export default Main = () =>{
+export default function Main (){
+    return (
+        <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
     
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
 } 
