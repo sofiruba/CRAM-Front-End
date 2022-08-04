b9c548bf99d377a76927035bc136ccf39f8a5f95
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-08-04 15:55:35 -0300

    perfil de lugar hardcodeado

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -1,41 +1,80 @@
 import React from 'react'
 import { View, Text, Button, StyleSheet, Image } from 'react-native'
 import { useNavigation } from "@react-navigation/native";
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Profile() {
     const navigation = useNavigation();
     return (
         <View style={styles.pag}>
-            <View >
+
+            <View>
+                <Image style={styles.img} source={require('../assets/Restaurante-ejemplo.jpg')} />
                 <Text style={styles.title}> Le Pain Quotidien   </Text>
                 <View >
-                    <Icon name={'heart'} style={styles.corazon} />
-                    <Text style={styles.subtitle}>Cafeteria</Text>
+                    <Text style={styles.subtitle}>Sobre el lugar</Text>
+                    <Text style={styles.parrafo}>mas informacion</Text>
+                </View>
+                <View style={styles.column}>
+                    <View style={styles.row}>
+                        <Text style={styles.elements}>Descuentos disponibles</Text>
+                        <Text style={styles.elements}>Agregar a lista</Text>
+                    </View>
+                    <Image style={styles.tinyimg} source={require('../assets/Mapa-Ejemplo.PNG')} />
                 </View>
                 <View>
-                    <Image source={require('../assets/Restaurante-ejemplo.jpg')} />
+                    <Button title="Ir a home" color="#DE95DB" onPress={() => navigation.goBack()} />
                 </View>
             </View>
-            <Button title="Ir a home" color="#DE95DB" onPress={() => navigation.goBack()} />
+
+
+
         </View>
     );
 
 }
 
 const styles = StyleSheet.create({
+    img: {
+        height: '90%',
+        width: '90%',
+        marginTop: '10%',
+        justifyContent: 'center',
+    },
+    tinyimg:{
+        height: '100%',
+        width: '100%',
+        justifyContent: 'left',
+    },
     title: {
         fontSize: 50,
         textAlign: 'center',
-        fontWeight: 'semibold',
+        fontWeight: 'bold',
     },
     pag: {
-        backgroundColor: "#F5D2F6",
+        backgroundColor: "#E5E5E5",
         height: '100%',
         width: '100%',
+        padding: '10%',
     },
-    subtitle:{
-        fontWeight: 'bold',
+    subtitle: {
+        fontWeight: 'semibold',
         fontSize: 20,
+    },
+    parrafo: {
+        fontWeight: 400,
+        fontSize: 15,
+    },
+    row: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+    },
+    elements: {
+        marginRight:'1%',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+    column: {
+        flexDirection: "column",
+        flexWrap: "wrap",
     },
 })
