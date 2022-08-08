f006fbfd060fd910c34940b68a2080e34b3ecac9
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-08-08 11:02:12 -0300

    Buscador con meli

--- src/components/buscador.js
+++ src/components/buscador.js
@@ -1,14 +1,8 @@
-import React from 'react'
-import { TextInput, View, StyleSheet, Text } from 'react-native';
-import Icon  from 'react-native-vector-icons/Ionicons';
+import { TextInput, View, StyleSheet } from 'react-native';
 export default function Buscador() {
     return (
         <View style={styles.busca}>
-<<<<<<< HEAD
-            <TextInput  placeholder="        Buscar... ">  </TextInput>
-=======
             <TextInput  style={styles.text} placeholder=" Buscar..."/>
->>>>>>> c60c22df9b393360eec15a27ab75a0dbcc0454b5
         </View>
     )
 }
@@ -23,26 +17,11 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         shadowColor: "7% 7% 5% rgba(0, 0, 0, 0.1)",
         fontSize: 14,
-<<<<<<< HEAD
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: '8%',
-        textAlign: 'center',
-      
-    },
-    icon: {
-        color: 'white',
-        width: '15%',
-=======
         margin: '9%',
         marginBottom: '15%',
     },
     text:{
         marginTop: '1%',
         marginLeft: '2%',
->>>>>>> c60c22df9b393360eec15a27ab75a0dbcc0454b5
     }
 })
-
-
-//* FILTROS FUENTE:Shippori Antique
